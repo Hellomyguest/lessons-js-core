4660a63d3f71a9a9e23547f93ed181a1151b7c09
AIllarionov <hellomyguest@yandex.ru>
2022-08-23 17:58:20 +0300

    added settings.css

--- src/script.js
+++ src/script.js
@@ -8,17 +8,17 @@ button.addEventListener("click", () => {
 
 const style = getComputedStyle(document.body);
 const mainColor = style.getPropertyValue("--day-white-color");
-let isDarkThemeEnabled = mainColor === " #FFFFFF";
+let isLightThemeOn = mainColor === " #FFFFFF";
 
 const lightThemeSwitcher = document.querySelector(".button_light-theme");
 const darkThemeSwitcher = document.querySelector(".button_dark-theme");
 
 darkThemeSwitcher.addEventListener("click", () => {
-  if (isDarkThemeEnabled) {
+  if (!isLightThemeOn) {
     document.body.style.setProperty("--day-white-color", " #2B2D33");
     document.body.style.setProperty("--label-color", " #171B1F");
     document.body.style.setProperty("--main-black-text-color", " #BCC4CC");
-    isDarkThemeEnabled = false;
+    isLightThemeOn = true;
     darkThemeSwitcher.classList.toggle("button_color_reverse-blue");
     darkThemeSwitcher.classList.toggle("button_color_blue");
     lightThemeSwitcher.classList.toggle("button_color_reverse-blue");
@@ -27,11 +27,11 @@ darkThemeSwitcher.addEventListener("click", () => {
 });
 
 lightThemeSwitcher.addEventListener("click", () => {
-  if (!isDarkThemeEnabled) {
+  if (isLightThemeOn) {
     document.body.style.setProperty("--day-white-color", " #FFFFFF");
     document.body.style.setProperty("--label-color", " #EBF0F5");
     document.body.style.setProperty("--main-black-text-color", " #000000");
-    isDarkThemeEnabled = true;
+    isLightThemeOn = false;
     darkThemeSwitcher.classList.toggle("button_color_reverse-blue");
     darkThemeSwitcher.classList.toggle("button_color_blue");
     lightThemeSwitcher.classList.toggle("button_color_reverse-blue");
